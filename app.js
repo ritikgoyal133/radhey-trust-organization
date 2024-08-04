e313ab8ebfec3c0b8019a81a1275a39cdf1fef29
Ritik Goyal <ritikgoyal3092000@gmail.com>
2024-08-04 10:22:12 +0530

    Initial Project Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
